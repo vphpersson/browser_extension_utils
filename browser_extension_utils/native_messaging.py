70dd7d2740bf9040c606f03ac0aa6d463f4b1fa5
Viktor Persson <vph.persson@gmail.com>
2022-05-21 16:23:42 +0200

    Initial.

---
+++ browser_extension_utils/native_messaging.py
@@ -0,0 +1,54 @@
+from sys import stdout, stdin
+from struct import pack as struct_pack, unpack as struct_unpack
+from typing import Optional, AnyStr
+
+NUM_MESSAGE_LENGTH_SPECIFIER_BYTES = 4
+
+
+def _make_outgoing_message_bytes(message_bytes: bytes) -> bytes:
+    """
+    Format the outgoing message's bytes.
+
+    The message bytes are preceded by bytes indicating the length of the message.
+
+    :param message_bytes: Unformatted message bytes.
+    :return: Formatted message bytes.
+    """
+
+    return struct_pack('=I', len(message_bytes)) + message_bytes
+
+
+def _write_message_bytes(message_bytes: AnyStr) -> int:
+    write_return_value: int = stdout.buffer.write(message_bytes)
+    stdout.buffer.flush()
+    return write_return_value
+
+
+def write_message(message: AnyStr, encoding: str = 'utf-8') -> int:
+    """
+    Write a message to be read by the browser extension to stdout.
+
+    :param message: The message to be written, as bytes or a string.
+    :param encoding: The encoding to be used in case the message is a string.
+    :return: The number of bytes written.
+    """
+
+    return _write_message_bytes(
+        message_bytes=_make_outgoing_message_bytes(
+            message_bytes=message.encode(encoding=encoding) if isinstance(message, str) else message
+        )
+    )
+
+
+def read_message() -> Optional[bytes]:
+    """
+    Read a message passed by the browser extension from stdin.
+
+    :return: The bytes constituting the message passed by the browser extension.
+    """
+
+    message_length_bytes: bytes = stdin.buffer.read(NUM_MESSAGE_LENGTH_SPECIFIER_BYTES)
+    if not message_length_bytes:
+        return None
+
+    return stdin.buffer.read(struct_unpack('=I', message_length_bytes)[0])
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='browser_extension_utils',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[]
+)
